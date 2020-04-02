d120ce1b5ef1f0da93d4e411d0d091a2e5ccc340
scurry222 <scurry222@gmail.com>
2020-04-02 16:21:41 -0700

    update state of selected language

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -2,13 +2,28 @@ import React from 'react'
 
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+    updateLanguage (selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        })
+    }
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
         return(
             <ul className='flex-center'>
                 {languages.map((language) => (
                     <li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button className="btn-clear nav-link"
+                        style={language === this.state.selectedLanguage ? {color: 'rgb(255, 175, 55)'} : null}
+                        onClick={() => this.updateLanguage(language)}>
                             {language}
                         </button>
                     </li>
